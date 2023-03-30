2d9bad921a6d96d4232b5cd66ad607464afb69fc
syyun <yunsuyeon@outlook.kr>
2023-03-30 15:09:10 -0400

    n개의 Text

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,12 @@ class App extends StatefulWidget {
 
 
 class _AppState extends State<App> {
-  int counter = 0;
+  List<int> numbers = [];
 
   void onClicked() {
     setState(() {
-      counter = counter + 1;
+      numbers.add(numbers.length);
+      print(numbers);
     });
   }
 
@@ -32,15 +33,11 @@ class _AppState extends State<App> {
                 fontSize: 30,
               ),
               ),
-              Text('$counter', style: const TextStyle(
-                fontSize: 30
-              ),
-              ),
+              for (var n in numbers) Text('$n'),
               IconButton(
                   iconSize: 40,
                   onPressed: onClicked,
-                  icon: const Icon(Icons.add_box_rounded
-                  ),
+                  icon: const Icon(Icons.add_box_rounded),
               ),
             ],
           ),
